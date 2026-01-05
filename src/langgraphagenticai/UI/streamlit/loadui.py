b2fec564cc8084841075c31f48f31707c8bb1a83
jitendra7337 <jitendrapp@yahoo.co.in>
2026-01-05 13:27:50 -0600

    commit 3

---
+++ src/langgraphagenticai/UI/streamlit/loadui.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import os
+
+from src.langgraphagenticai.UI.uiconfig import Config
---
+++ src/langgraphagenticai/UI/uiconfig.py
@@ -0,0 +1,21 @@
+from configparser import Configparser
+
+
+
+class Config:
+    def __init__(self,config_file="./src/langgraphagenticai/UI/uiconfig.ini"):
+        self.config=ConfigParser()
+        self.config.read(config_file)
+
+    def get_llmoptions(self):
+        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
+         
+    def get_usecaseoptions(self):
+        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
+
+
+    def get_usecasegroqmodeoptions(self):
+        return self.config["DEFAULT"].get("GROQ_MODE_OPTION").split(", ")
+
+    def get_pagetitleoptions(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE")   
